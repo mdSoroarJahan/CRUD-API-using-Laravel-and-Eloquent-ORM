d398dc2d381dec52ec1ecf69ca3a82af95a8563f
mdSoroarJahan <soroarjahan17@gmail.com>
2025-12-28 14:06:19 +0600

    Implement CRUD operations for Product model and configure Sanctum for API authentication

---
+++ CRUD_API/app/Http/Controllers/ProductController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        $products = Product::all();
+        return $products;
+    }
+
+    public function show(Request $request, $id)
+    {
+        $product = Product::find($id);
+        if (!$product) {
+            return response()->json(['message' => 'Product not found'], 404);
+        }
+        return response()->json($product);
+    }
+
+    public function store(Request $request)
+    {
+        $product = Product::create([
+            'name' => $request->name,
+            'description' => $request->description,
+            'price' => $request->price,
+            'stock' => $request->stock
+        ]);
+
+        return response()->json(['message' => 'Product created successfully.', 'product' => $product], 201);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $product = Product::find($id);
+        if (!$product) {
+            return response()->json(['message' => 'Product not found'], 404);
+        }
+
+        $product->update([
+            'name' => $request->name,
+            'description' => $request->description,
+            'price' => $request->price,
+            'stock' => $request->stock
+        ]);
+        return response()->json(['message' => 'Product updated successfully.', 'product' => $product]);
+    }
+
+    public function destroy(Request $request, $id)
+    {
+        $product = Product::find($id);
+        if (!$product) {
+            return response()->json(['message' => 'Product not found'], 404);
+        }
+        $product->delete();
+        return response()->json(['message' => 'Product deleted successfully']);
+    }
+}
